e18baeca3e20445c98f293031fdf3939168536e7
yto-tkg <yuto-takagi@cybozu.co.jp>
2022-09-23 16:23:57 +0900

    first commit

---
+++ src/main/java/com/md/editor/MdeditorApplication.java
@@ -0,0 +1,13 @@
+package com.md.editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdeditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdeditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/md/editor/MdeditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.md.editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdeditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
